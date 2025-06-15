9063a0e837e631533c3a240f9a58442750680b30
Nurdaulet Taumergenov <taumergenov@duck.com>
2025-06-15 13:18:35 +0900

    add: better parsing and test cases

--- test.py
+++ test.py
@@ -30,6 +30,56 @@ class DatabaseTest(unittest.TestCase):
         ]
         self.assertEqual(result, expected_result)
 
+    def test_prints_error_message_when_table_is_full(self):
+        script = [f"insert {i} user{i} person{i}@example.com" for i in range(1401)]
+        script.append(".exit")
+        result = self.run_script(script)
+        self.assertEqual(result[-2], 'db > Error: Table full.')
+
+    def test_allows_inserting_strings_that_are_the_maximum_length(self):
+        long_username = "a"*32
+        long_email = "a"*255
+        script = [
+                f"insert 1 {long_username} {long_email}",
+                "select",
+                ".exit",
+        ]
+        result = self.run_script(script)
+        self.assertEqual(result, [
+            "db > Executed.",
+            f"db > (1, {long_username}, {long_email})",
+            "Executed.",
+            "db > "
+        ])
+
+    def test_prints_error_message_if_strings_are_too_long(self):
+        long_username = "a"*33
+        long_email = "a"*256
+        script = [
+                f"insert 1 {long_username} {long_email}",
+                "select",
+                ".exit",
+        ]
+        result = self.run_script(script)
+        self.assertEqual(result, [
+            "db > String is too long.",
+            "db > Executed.",
+            "db > "
+        ])
+
+    def test_prints_an_error_message_if_id_is_negative(self):
+        script = [
+                "insert -1 nurda nurda@nurda.com",
+                "select",
+                ".exit"
+        ]
+        result = self.run_script(script)
+        self.assertEqual(result, [
+            "db > ID must be positive.",
+            "db > Executed.",
+            "db > ",
+        ])
+
 if __name__ == "__main__":
     unittest.main()
 
